3d171a0cd672d5f184da32c7fd6bab97294b9c3d
Aimmzz <rohimkurniawan456@gmail.com>
2024-03-18 09:18:04 +0700

    make search news response

--- app/src/main/java/com/aimcode/luarberita/ui/fragment/SearchNewsFragment.kt
+++ app/src/main/java/com/aimcode/luarberita/ui/fragment/SearchNewsFragment.kt
@@ -1,18 +1,103 @@
 package com.aimcode.luarberita.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.aimcode.luarberita.R
+import com.aimcode.luarberita.data.local.ArticleDatabase
+import com.aimcode.luarberita.databinding.FragmentSearchNewsBinding
+import com.aimcode.luarberita.reepository.NewsRepository
 import com.aimcode.luarberita.ui.MainActivity
+import com.aimcode.luarberita.ui.adapter.NewsAdapter
 import com.aimcode.luarberita.ui.viewmodel.NewsViewModel
+import com.aimcode.luarberita.util.Constants.Companion.SEARCH_NEWS_TIME_DELAY
+import com.aimcode.luarberita.util.NewsViewModelProviderFactory
+import com.aimcode.luarberita.util.Resource
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
 
-    lateinit var viewModel: NewsViewModel
+    private var _binding: FragmentSearchNewsBinding? = null
+    private val binding get() = _binding!!
+
+    // Ubah menjadi deklarasi viewModel di sini
+    private lateinit var viewModel: NewsViewModel
+
+    private lateinit var newsAdapter: NewsAdapter
+    private val TAG = "BreakingNewsFragment:"
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        viewModel = (activity as MainActivity).viewModel
+        _binding = FragmentSearchNewsBinding.bind(view)
+
+        // Inisialisasi viewModel langsung di sini
+        val newsRepository = NewsRepository(ArticleDatabase(requireContext()))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
+
+        setupRecyclerView()
+
+        //corotine untuk menghandle pencari supaya agak delay
+        var job: Job? = null
+        binding.etSearch.addTextChangedListener { editable ->
+            job?.cancel()
+            job = MainScope().launch {
+                delay(SEARCH_NEWS_TIME_DELAY)
+                editable.let {
+                    if (editable.toString().isNotEmpty()) {
+                        viewModel.searchNews(editable.toString())
+                    }
+                }
+            }
+        }
+
+        viewModel.searchNews.observe(viewLifecycleOwner, Observer { response ->
+            when(response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e(TAG, "An error occured: $message")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        })
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        binding.rvSearchNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
+    private fun hideProgressBar() {
+        binding.paginationProgressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        binding.paginationProgressBar.visibility = View.VISIBLE
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/aimcode/luarberita/util/Constants.kt
+++ app/src/main/java/com/aimcode/luarberita/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val  API_KEY = "e77b810d384842c2941b4a33dcdb256b"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
